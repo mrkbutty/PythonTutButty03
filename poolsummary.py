194b84c987c404528411111b83995c79f295d21c
Butty <mark@markbutterworth.net>
2019-05-13 16:26:58 +0100

    updated plan

--- poolsummary.py
+++ poolsummary.py
@@ -4,9 +4,9 @@
 
 # Plan:
 '''
-1) Read poolinfo.txt file and summarize the contents.
+1) Read poolinfo.txt file and summarize the contents printing the overview only to the screen.
 Note structure is;
-Section headings for each pool type;  DP, DP(Multi-Tier), DP(?????), TI
+Section headings for each pool type;  DP, DP(Multi-Tier), DP(?????), TI are followed by (n x pool) details.
 First pool overview starts with "POOL-ID : 0x????( ?)" (note the seperating ":")
 Also pool overview shows unused items values as "0[MB]" or "0[%]" or "No Setting" or "No Data" 
 Each pool detail list is seperated by a blank line
@@ -22,13 +22,22 @@ def main():
     with open('testdata\poolinfo_150_470282.txt') as fd:
         lines = fd.readlines()
 
-    print(f'number of lines = {len(lines)}')
+    section = 0
     for i, text in enumerate(lines):
         text = text.strip()
-        print(text)
-        if i > 4: 
-            break
-    
+
+        if text == 'DP' or text == 'TI':
+            section = 1
+            sectiontype = text
+
+        if text == '':
+            section = 0
+
+        if section > 0 and not('No data' in text) and not('No Setting' in text) \
+            and not('0[MB]' in text) and not('0[%]' in text):
+            print(text)
+
+
     return 0
     
 
